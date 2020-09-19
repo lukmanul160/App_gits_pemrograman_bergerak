c35b19acc783250af213d39c4459a53c2fd25eee
lukmanul160 <lukmanul160@gmail.com>
2020-09-19 11:12:15 +0700

    commit 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private String A;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
