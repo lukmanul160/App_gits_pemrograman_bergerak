54eab3af90563375a72e946fe7c0fb61b5d444f6
lukmanul160 <lukmanul160@gmail.com>
2020-09-22 23:29:40 +0700

    commit 2 add onboardlanding

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,133 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private OnboardingAdapter onboardingAdapter;
+    private LinearLayout layoutOnboardingIndicators;
+    private MaterialButton buttonOnboardingAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        layoutOnboardingIndicators = findViewById(R.id.layoutOnboardingIndicators);
+        buttonOnboardingAction = findViewById(R.id.buttonOnboardingAction);
+
+
+        setupOnboardingItem();
+
+        final ViewPager2 onboardingViewPager = findViewById(R.id.onBoardingViewPager);
+        onboardingViewPager.setAdapter(onboardingAdapter);
+
+        setupOnboardingIndicators();
+        setCurrentOnboardingIndicators(0);
+
+        onboardingViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentOnboardingIndicators(position);
+            }
+        });
+
+        buttonOnboardingAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(onboardingViewPager.getCurrentItem()+1 < onboardingAdapter.getItemCount()){
+                    onboardingViewPager.setCurrentItem(onboardingViewPager.getCurrentItem()+1);
+                }else{
+                    Intent explicit = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(explicit);
+                    finish();
+                }
+            }
+        });
+
     }
+
+    private void setupOnboardingItem(){
+        List<OnboardingItem> onboardingItems = new ArrayList<>();
+
+        OnboardingItem itemtitle = new OnboardingItem();
+        itemtitle.setTitle("1");
+        itemtitle.setDescription("1");
+        itemtitle.setImage(R.drawable.gambar);
+
+        OnboardingItem itemdesc = new OnboardingItem();
+        itemdesc.setTitle("2");
+        itemdesc.setDescription("2");
+        itemdesc.setImage(R.drawable.gambar);
+
+        OnboardingItem itemimg = new OnboardingItem();
+        itemimg.setTitle("3");
+        itemimg.setDescription("3");
+        itemimg.setImage(R.drawable.gambar);
+
+        onboardingItems.add(itemtitle);
+        onboardingItems.add(itemdesc);
+        onboardingItems.add(itemimg);
+
+        onboardingAdapter = new OnboardingAdapter(onboardingItems);
+
+
+    }
+
+    private void setupOnboardingIndicators(){
+        ImageView[] indicators = new ImageView[onboardingAdapter.getItemCount()];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+
+        layoutParams.setMargins(8,0,8,0);
+
+        for(int i=0; i < indicators.length;i++){
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),
+                    R.drawable.onboarding_indicator_inactive
+            ));
+            indicators[i].setLayoutParams(layoutParams);
+            layoutOnboardingIndicators.addView(indicators[i]);
+        }
+    }
+
+    private void setCurrentOnboardingIndicators(int index) {
+        int childCount = layoutOnboardingIndicators.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            ImageView imageView = (ImageView) layoutOnboardingIndicators.getChildAt(i);
+            if (i == index) {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(
+                                getApplicationContext(), R.drawable.onboarding_indicator_active));
+            } else {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(
+                                getApplicationContext(), R.drawable.onboarding_indicator_inactive)
+                );
+            }
+        }
+        if(index ==onboardingAdapter.getItemCount()-1){
+            buttonOnboardingAction.setText("Start");
+        }else{
+            buttonOnboardingAction.setText("Next");
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/myapplication/OnboardingAdapter.java
@@ -0,0 +1,63 @@
+package com.example.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class OnboardingAdapter extends RecyclerView.Adapter<OnboardingAdapter.OnboardingViewHolder> {
+
+    private List<OnboardingItem> onboardingItems;
+
+    public OnboardingAdapter(List<OnboardingItem> onboardingItems) {
+        this.onboardingItems = onboardingItems;
+    }
+
+    @NonNull
+    @Override
+    public OnboardingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new OnboardingViewHolder(
+                LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.item_container_onboarding,parent,false
+                ));
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OnboardingViewHolder holder, int position) {
+        holder.setImageOnboardingData(onboardingItems.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return onboardingItems.size();
+    }
+
+// ----------------------------------------------------------------
+    class OnboardingViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView textTitle;
+        private TextView textDescription;
+        private ImageView imageOnboarding;
+
+        public OnboardingViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textTitle = itemView.findViewById(R.id.textTitle);
+            textDescription = itemView.findViewById(R.id.textDescription);
+            imageOnboarding = itemView.findViewById(R.id.imageOnboarding);
+        }
+
+        void setImageOnboardingData(OnboardingItem onboardingItem){
+            textTitle.setText(onboardingItem.getTitle());
+            textDescription.setText(onboardingItem.getDescription());
+            imageOnboarding.setImageResource(onboardingItem.getImage());
+        }
+    }
+}
